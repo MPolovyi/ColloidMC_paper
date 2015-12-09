e31e94fd1c4693c743133c2e4a3511a088921abb
mpolovyi <m.polovyi@outlook.com>
2015-12-09 22:16:14 +0000

    working on

--- rplotter/DrawCorrelations.R
+++ rplotter/DrawCorrelations.R
@@ -1,29 +1,44 @@
 require(RMongo)
 source("~/Documents/MiscColloidFunctions.R")
 
+try(dyn.unload("/home/mpolovyi/.CLion12/system/cmake/generated/781f67f/781f67f/Release/libDecodeBinary.so"))
+dyn.load("/home/mpolovyi/.CLion12/system/cmake/generated/781f67f/781f67f/Release/libDecodeBinary.so")
+
+ptCount <- 500
+rho <- 1
+
+collectionName <- "dipole_upd"
 mdb <- mongoDbConnect('MarshallDB')
-#f <- dbGetDistinct(mdb, "dipole", "InteractionForce", '{"ParticleCount": 5000}')
+f <- dbGetDistinct(mdb, paste(c(collectionName, "simulation_stats"), collapse = "_"), "InteractionForce",
+                   paste(c('{',
+                           '"Density":', sprintf("%.56f", rho),
+                           ', "ParticleCount":' , sprintf("%.56f", ptCount),
+                           '}'), collapse = " "))
 
-Sweeps <- c(500000, 1000000, 1500000, 2000000, 2500000, 3000000, 3500000, 4000000, 4500000, 5000000, 5500000)
+Sweeps <- dbGetDistinct(mdb, paste(c(collectionName, "simulation_stats"), collapse = "_"), "Sweeps", 
+                        paste(c('{',
+                                '"Density":', sprintf("%.56f", rho),
+                                ', "ParticleCount":' , sprintf("%.56f", ptCount),
+                                '}'), collapse = " "))
 
-corrCount <- 20
-pptCount <- 5000
+Sweeps <- c(4500000, 5000000, 5500000)
+
+corrCount <- 150
 corrs <- rep(0, corrCount)
 counts <- rep(1, corrCount)
 
-SystemSize <- 1.215185968820742*ptCount*2
-maxCorrLength <- 30
+SystemSize <- 0.607593*ptCount/rho
+maxCorrLength <- 100
 corrLength <- seq(0, maxCorrLength, length = corrCount)
 
 for (sw in Sweeps){
-  print(sw)
-  
-  q <- dbGetQueryForKeys(mdb, "dipole",
+  q <- dbGetQueryForKeys(mdb, "dipole_upd",
                          paste(c('{',
-                                 '"ParticleCount":' , 5000,
+                                 '"ParticleCount":' , ptCount,
                                  ', "InteractionForce":' , sprintf("%.53f", f[findNearestIndex(1, f)]),
                                  #', "Sweeps": {"$mod": [500000, 0]}',
                                  ', "Sweeps":', sprintf("%.53f", sw),
+                                 ', "Density":', rho,
                                  '}'), collapse = " "),
                          paste(c('{', 
                                '"ParticleCoordinates":', 1, 
@@ -32,34 +47,40 @@ for (sw in Sweeps){
                          skip = 0,
                          limit = 100000)
   
+  print(sw)
   
-  for(ind in 1:length(q$ParticleCoordinates)){
+  for(ind in 1:5){ #:length(q$ParticleCoordinates)){
+    rotations <- rep(0, ptCount)
+    .C("Function_UnwrapBinary",
+       as.character(q$ParticleRotationZ[ind]),
+       as.integer(ptCount),
+       as.double(rotations))[[3]] -> rotations
     
-    rotations <- q$ParticleRotationZ[ind]
-    rotations <- gsub("\\[", "", rotations)
-    rotations <- gsub("\\]", "", rotations)
-    rotations <- as.numeric(unlist(strsplit(rotations, split=",")))
+    coords <- rep(0, ptCount)
+    .C("Function_UnwrapBinary",
+       as.character(q$ParticleCoordinates[ind]),
+       as.integer(ptCount),
+       as.double(coords))[[3]] -> coords
     
-    coords <- q$ParticleCoordinates[ind]
-    coords <- gsub("\\]", "", coords)
-    coords <- gsub("\\[", "", coords)
-    coords <- as.numeric(unlist(strsplit(coords, split=",")))
+    print(ind)
     
-    ptCount <- length(coords)
     for (i in seq_along(coords)){
+      
       ptCoordinate <- coords[i]
       ptRotation <- rotations[i]
       
-      for (j in seq_along(coords)){
-        ptNextCoordinate <- coords[getNext(j, ptCount)]
-        ptNextRotation <- rotations[getNext(j, ptCount)]
+      j <- i
+      corrs[1] <- corrs[1] + ptRotation^2
+      counts[1] <- counts[1] + 1
+      dst <- 0
+      while (dst < maxCorrLength) {
+        j <- getNext(j, ptCount)
+        ptNextCoordinate <- coords[j]
+        ptNextRotation <- rotations[j]
         
         dst <- getDistanceRight(ptCoordinate, ptNextCoordinate, SystemSize)
         
-        if(dst > maxCorrLength){
-          break
-        }
-        nearest <- findNearesIndex(dst, corrLength)
+        nearest <- findNearestIndex(dst, corrLength)
         corrs[nearest] = corrs[nearest] + ptRotation*ptNextRotation
         counts[nearest] = counts[nearest] + 1
       }
@@ -67,3 +88,5 @@ for (sw in Sweeps){
   }
 }
 corrs <- corrs/counts
+plot(corrLength, corrs, lwd = 2, pch = 1, col = 1)
+# legend("topright", legend = c("0.5", "1", "1.5"), col = c(1, 2, 3), pch = c(1, 2, 3), lty = 0, lwd = 2)
