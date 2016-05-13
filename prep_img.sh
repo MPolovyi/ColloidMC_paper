822716961644a3346c0dd4a7c3eaa4ffb4b7bed1
mpolovyi <m.polovyi@outlook.com>
2016-05-13 19:28:05 +0100

    Major changes with latest results

--- prep_img.sh
+++ prep_img.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-(find . -type f -name "*.eps" -exec \cp -vuni '{}' "./Images" ";") &> /dev/null
-(find . -type f -name "*.png" -exec \cp -vuni '{}' "./Images" ";") &> /dev/null
+(find . -type f -name "*.eps" -exec \cp -vuni '{}' "./Images" ";") #&> /dev/null
+(find . -type f -name "*.png" -exec \cp -vuni '{}' "./Images" ";") #&> /dev/null
